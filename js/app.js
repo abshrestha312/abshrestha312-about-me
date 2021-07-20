371984dba70392530f80dcc2a826d3e7feea831c
Abinash Shrestha <75637766+abshrestha312@users.noreply.github.com>
2021-07-20 09:32:29 -0500

    About Me

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 'use strict';
-// prints the value within the parens to the console (browser in this case)
 let userName = prompt('What is your name?');
-const randNum = Math.floor(Math.random() * 100);
+function getRandomizer(bottom, top) {
+  return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
+}
+const randNum = getRandomizer( 1, 100 );
+//const randNum = Math.floor(Math.random() * 100);
 const questions = ['Am I 6ft tall?', 'Do I live in Seattle?', 'Do I like to sing?', 'Do I like pizza?', 'Can I swim?', 'Guess a number between 1 and 100. You have 4 attempts, good luck!', 'What is one of my favorite hobbies?'];
-const response = ['n', 'n', 'y', 'y', 'n'. randNum, 'guitar', 'singing', 'movies', 'gardening', 'painting', 'shopping', 'traveling', 'cooking', 'hiking', 'driving'];
+const response = ['n', 'n', 'y', 'y', 'n', randNum, 'guitar', 'singing', 'movies', 'gardening', 'painting', 'shopping', 'traveling', 'cooking', 'hiking', 'driving'];
 const genericPositive = 'Congratulations, you got the answer right!';
 const genericNegative = 'Sorry, you got the answer wrong.';
 const genericInvalid = 'Your answer did not fit the criteria';
